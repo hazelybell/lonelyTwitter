b63bbb049ee007dd8ccbb9e0d9e3c37bd813f045
joshua2uaExample <joshua2@cs.ualberta.ca>
2014-01-14 17:35:13 -0700

    begin model class

---
+++ src/ca/ualberta/cs/lonelytwitter/lonelyTweetModel.java
@@ -0,0 +1,20 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.util.Date;
+
+public class lonelyTweetModel {
+	private String text;
+	private Date timestamp;
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public Date getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(Date timestamp) {
+		this.timestamp = timestamp;
+	}
+}
