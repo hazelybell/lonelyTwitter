f81b06550d55133ad0de91d2f56fc77ab2faec7d
joshua2uaExample <joshua2@cs.ualberta.ca>
2014-01-16 15:14:07 -0700

    Subclassing for fun and profit

---
+++ src/ca/ualberta/cs/lonelytwitter/ImportantTweetModel.java
@@ -0,0 +1,19 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.util.Date;
+
+public class ImportantTweetModel extends LonelyTweetModel {
+
+	public ImportantTweetModel(String text, Date timestamp) {
+		super(text, timestamp);
+	}
+
+	public ImportantTweetModel(String text) {
+		super(text);
+	}
+	
+	public String getText() {
+		return "Important! " + super.getText();
+	}
+
+}
