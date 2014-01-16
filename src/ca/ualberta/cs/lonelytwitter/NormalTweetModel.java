d8cca65b0e81999a186f9659c388c93a041c7078
joshua2uaExample <joshua2@cs.ualberta.ca>
2014-01-16 15:49:50 -0700

    add normal tweet model

---
+++ src/ca/ualberta/cs/lonelytwitter/NormalTweetModel.java
@@ -0,0 +1,17 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.util.Date;
+
+public class NormalTweetModel extends LonelyTweetModel {
+
+	public NormalTweetModel(String text) {
+		super(text);
+	}
+
+	@Override
+	public Date getTimestamp() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
