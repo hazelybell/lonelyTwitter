0c652600c25eefd966ac04129ebd83fd4d866b47
joshua2uaExample <joshua2@cs.ualberta.ca>
2014-01-16 14:57:37 -0700

    Constructors

--- src/ca/ualberta/cs/lonelytwitter/LonelyTweetModel.java
+++ src/ca/ualberta/cs/lonelytwitter/LonelyTweetModel.java
@@ -8,14 +8,28 @@ public class LonelyTweetModel {
 	public String getText() {
 		return text;
 	}
-	public void setText(String text) {
+	
+	public LonelyTweetModel(String text) {
+		super();
 		this.text = text;
+		timestamp = new Date();
 	}
-	public Date getTimestamp() {
-		return timestamp;
-	}
-	public void setTimestamp(Date timestamp) {
+	
+	public LonelyTweetModel(String text, Date timestamp) {
+		super();
+		this.text = text;
 		this.timestamp = timestamp;
 	}
 	
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	public Date getTimestamp() {
+		return timestamp;
+	}
+	
+	public void setTimestamp(Date timestamp) {
+		this.timestamp = timestamp;
+	}
 }
