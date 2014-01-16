dbc51d765d202640fb7499abeff92dd69fdc4768
Joshua Charles Campbell <gitcommit@orezpraw.com>
2014-01-16 13:52:19 -0700

    Revert "Constructor overloading"
    This reverts commit 8c90f692769c1ec51382adab0022bce43fe79ee2.

--- src/ca/ualberta/cs/lonelytwitter/lonelyTweetModel.java
+++ src/ca/ualberta/cs/lonelytwitter/lonelyTweetModel.java
@@ -17,16 +17,4 @@ public class lonelyTweetModel {
 	public void setTimestamp(Date timestamp) {
 		this.timestamp = timestamp;
 	}
-	
-	public lonelyTweetModel(String text, Date timestamp) {
-		super();
-		this.text = text;
-		this.timestamp = timestamp;
-	}
-	public lonelyTweetModel(String text) {
-		super();
-		this.text = text;
-		this.timestamp = new Date();
-	}
-	
 }
