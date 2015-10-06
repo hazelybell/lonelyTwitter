a9c89204dd6afcfa0d8849596038989734907d8e
kstem <kstem@users.noreply.github.com>
2015-10-06 12:19:51 -0600

    Update TweetListTest.java

--- app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/TweetListTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/TweetListTest.java
@@ -1,74 +1,112 @@
 package ca.ualberta.cs.lonelytwitter;
 
-import android.test.ActivityInstrumentationTestCase2;
-
-import junit.framework.TestCase;
-
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
-/**
- * Created by kstember on 9/29/15.
- */
-public class TweetListTest extends ActivityInstrumentationTestCase2 {
-    private ArrayList<Tweet> tweets = new ArrayList<Tweet>();
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
-    public TweetListTest() {
-        super(ca.ualberta.cs.lonelytwitter.LonelyTwitterActivity.class);
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+public class LonelyTwitterActivity extends Activity {
+
+	private static final String FILENAME = "file.sav";
+	private EditText bodyText;
+	private ListView oldTweetsList;
+	private ArrayList<Tweet> tweets = new ArrayList<Tweet>();
+	private ArrayAdapter<Tweet> adapter;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState); // view
+		setContentView(R.layout.main);      // view
+
+		bodyText = (EditText) findViewById(R.id.body);               // view
+		Button saveButton = (Button) findViewById(R.id.save);        // view
+      //Button clearButton = (Button) findViewById(R.id.clear);      // view
+		oldTweetsList = (ListView) findViewById(R.id.oldTweetsList); // view
+
+		saveButton.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View v) {
+				setResult(RESULT_OK);
+                String text = bodyText.getText().toString();  // controller - changes stuff for user
+				tweets.add(new NormalTweet(text));            // controller - changes stuff for user
+				adapter.notifyDataSetChanged();               // view -- doesn't notify of any changes
+                saveInFile();                                 // model - changes stuff on disk, not for user
+			}
+		});
+
+      /*clearButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                setResult(RESULT_OK);
+               // tweets.deleteFile(FILENAME);
+                tweets.clear();                              // controller
+                adapter.notifyDataSetChanged();              // view
+                saveInFile();                                // model
+
+            }
+        });
+*/
+
+	}
+
+    @Override
+    protected void onStart() {
+        // TODO Auto-generated method stub
+        super.onStart();                                                       // model
+        loadFromFile();                                                        // model
+        adapter = new ArrayAdapter<Tweet>(this, R.layout.list_item, tweets);   // model
+        oldTweetsList.setAdapter(adapter);                                     // model
     }
 
-    public void testHoldsStuff() {
-        TweetList list = new TweetList();
-        Tweet tweet = new NormalTweet("test");
-        list.add(tweet); //fails originally b/c no add method
-        assertSame(list.getMostRecentTweet(), tweet);
+    private void loadFromFile() {  // entire function is model, only adjusting things on disk
+        try {
+            FileInputStream fis = openFileInput(FILENAME);
+            BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+            Gson gson = new Gson();
+            // Taken from https://google-gson.googlecode.com/svn/trunk/gson/docs/javadocs/com/google/gson/Gson.html 2015-09-22
+            Type listType = new TypeToken<ArrayList<NormalTweet>>() {}.getType();
+            tweets = gson.fromJson(in, listType);
+        } catch (FileNotFoundException e) {
+            tweets = new ArrayList<Tweet>();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public void testHoldsManyThings() {
-        TweetList list = new TweetList();
-        Tweet tweet = new NormalTweet("test");
-        list.add(tweet);
-        assertEquals(list.count(), 1);
-        list.add(new NormalTweet("test"));
-        assertEquals(list.count(), 2);
+    private void saveInFile() { // entire function is model
+        try {
+            FileOutputStream fos = openFileOutput(FILENAME,
+                    0);
+            OutputStreamWriter writer = new OutputStreamWriter(fos);
+            Gson gson = new Gson();
+            gson.toJson(tweets, writer);
+            writer.flush();
+            fos.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
-
-
-
-
-    public void testGetTweets() {
-        ArrayList<Tweet> tweets2 = new ArrayList<Tweet>();
-        TweetList list = new TweetList();
-        Tweet tweet1 = new NormalTweet("catScan");
-        Tweet tweet2 = new NormalTweet("dogScan");
-        list.add(tweet1);
-        list.add(tweet2);
-        tweets2.add(tweet1);
-        tweets2.add(tweet2);
-        tweets = list.getTweets();
-        assertEquals(tweets, tweets2);
-    }
-
-    public void testRemoveTweet() {
-        TweetList list = new TweetList();
-        Tweet tweet = new NormalTweet("catScan");
-        list.add(tweet);
-        list.removeTweet(tweet);
-        assertEquals(false, list.hasTweet(tweet));
-    }
-    public void testGetCount() {
-        TweetList list = new TweetList();
-        Tweet tweet = new NormalTweet("test");
-        list.add(tweet);
-        assertEquals(list.count(), 1);
-        list.add(new NormalTweet("test"));
-        assertEquals(list.count(), 2);
-    }
-    public void testAddTweet() {
-        TweetList list = new TweetList();
-        Tweet tweet = new NormalTweet("test");
-        assertEquals(false, list.addTweet(tweet));
-
-       }
-
-
 }
