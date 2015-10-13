08271883c7e36eb876dcc4e33c882bb8a774d5e2
kstem <kstem@users.noreply.github.com>
2015-10-13 12:40:24 -0600

    update test

--- app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivityTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivityTest.java
@@ -1,14 +1,22 @@
 package ca.ualberta.cs.lonelytwitter;
 
-import android.app.Activity;
-import android.test.ActivityInstrumentationTestCase2;
+        import android.app.Activity;
+        import android.app.Instrumentation;
+        import android.test.ActivityInstrumentationTestCase2;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.ListView;
 
-import junit.framework.TestCase;
+        import junit.framework.TestCase;
 
 /**
  * Created by wz on 14/09/15.
  */
 public class LonelyTwitterActivityTest extends ActivityInstrumentationTestCase2 {
+
+    private EditText bodyText;
+    private Button saveButton;
 
     public LonelyTwitterActivityTest() {
         super(ca.ualberta.cs.lonelytwitter.LonelyTwitterActivity.class);
@@ -16,6 +24,90 @@ public class LonelyTwitterActivityTest extends ActivityInstrumentationTestCase2 
 
     public void testStart() throws Exception {
         Activity activity = getActivity();
+
+    }
+
+
+    public void testEditATweet() {
+        // starts lonelyTwitter
+        LonelyTwitterActivity activity = (LonelyTwitterActivity) getActivity();
+        // reset the app to a known state
+        activity.getTweets().clear();
+
+        // user clicks on tweet they want to edit
+        bodyText = activity.getBodyText();
+
+        activity.runOnUiThread(new Runnable() {
+            public void run() {
+                bodyText.setText("hamburgers");
+            }
+        });
+        getInstrumentation().waitForIdleSync(); // make sure our UI thread finished
+
+        saveButton = activity.getSaveButton();
+
+        activity.runOnUiThread(new Runnable() {
+            public void run() {
+                saveButton.performClick();
+            }
+        });
+        getInstrumentation().waitForIdleSync(); // make sure our UI thread finished
+
+        final ListView oldTweetsList = activity.getOldTweetsList();
+        Tweet tweet = (Tweet) oldTweetsList.getItemAtPosition(0);
+        assertEquals("hamburgers", tweet.getText());
+
+        activity.runOnUiThread(new Runnable() {
+            public void run() {
+                View v = oldTweetsList.getChildAt(0);
+                oldTweetsList.performItemClick(v, 0, v.getId());
+            }
+        });
+        getInstrumentation().waitForIdleSync(); // make sure our UI thread finished
+
+
+        // Following was stolen from https://developer.android.com/training/activity-testing/activity-functional-testing.html 2015-10-13
+        // Set up an ActivityMonitor
+        Instrumentation.ActivityMonitor receiverActivityMonitor =
+                getInstrumentation().addMonitor(EditTweetActivity.class.getName(),
+                        null, false);
+
+        // Validate that ReceiverActivity is started
+        EditTweetActivity receiverActivity = (EditTweetActivity)
+                receiverActivityMonitor.waitForActivityWithTimeout(1000);
+        assertNotNull("ReceiverActivity is null", receiverActivity);
+        assertEquals("Monitor for ReceiverActivity has not been called",
+                1, receiverActivityMonitor.getHits());
+        assertEquals("Activity is of wrong type",
+                EditTweetActivity.class, receiverActivity.getClass());
+
+        // Remove the ActivityMonitor
+        getInstrumentation().removeMonitor(receiverActivityMonitor);
+
+        // assert that the tweet being shown on the edit screen is tweet we clicked on
+        assertEquals(Tweet.getText, "hamburgers");
+
+        // edit the text of that tweet
+        activity.runOnUiThread(new Runnable() {
+            public void run() {
+                bodyText.setText("Old hamburgers");
+            }
+        });
+        getInstrumentation().waitForIdleSync();
+
+        // save our edits
+        Tweet tweet1 = new NormalTweet();
+        tweet1.saveInFile();
+
+        // assert edits were saved into tweet correctly
+
+
+        // assert that our edits are shown on the screen to user back in main activity
+
+        // end of test: clear the data
+        // end of test: make sure edit activity is closed
+        receiverActivity.finish();
+
 
     }
 }
