d0654960bf60bb4146f3e70cb86d66199c4af06d
KenRLi <krli@ualberta.ca>
2017-09-21 01:32:17 -0600

    Added all demo code from lab 2

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweetable.java
@@ -0,0 +1,12 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.util.Date;
+
+/**
+ * Created by Ken on 21-Sep-2017.
+ */
+
+public interface Tweetable {
+    String getMessage();
+    Date getDate();
+}
