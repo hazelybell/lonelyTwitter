866f151d7026fab19faabc093a40074a4d72db21
jtfwong <43226692+jtfwong@users.noreply.github.com>
2018-09-22 14:08:46 -0600

    Delete TweetTooLongException.java

--- TweetTooLongException.java
+++
@@ -1,8 +0,0 @@
-package ca.ualberta.cs.lonelytwitter;
-
-public class TweetTooLongException extends Exception {
-
-    TweetTooLongException() {
-        super("The message is too long! Please keep your tweets within 140 characters.");
-    }
-}
