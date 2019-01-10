2a3f53f0a8046ab0698ebdd887e185fb8c92553a
Ryan Jensen <rrjensen@cs.ualberta.ca>
2019-01-09 19:23:41 -0700

    Added some comments to the classes

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/Anger.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Anger.java
@@ -3,12 +3,14 @@ package ca.ualberta.cs.lonelytwitter;
 import java.util.Date;
 
 public class Anger extends Mood {
+    // Standard constructors for the class
     public Anger(){
         this.date = new Date(0);
     }
     public Anger(Date date){
         this.date = date;
     }
+    // Do the format. May need to add a String input, since otherwise this is no better than an emoticon
     public String format(){
         return "I am angry! :(";
     }
--- app/src/main/java/ca/ualberta/cs/lonelytwitter/Happy.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Happy.java
@@ -3,12 +3,14 @@ package ca.ualberta.cs.lonelytwitter;
 import java.util.Date;
 
 public class Happy extends Mood {
+    // Constructors, default day is epoch time
     public Happy(){
         this.date = new Date(0);
     }
     public Happy(Date date){
         this.date = date;
     }
+    // Do the format. May need to add a String input, since otherwise this is no better than an emoticon
     public String format(){
         return "I am happy! :)";
     }
